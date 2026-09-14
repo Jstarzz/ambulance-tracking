0213804e7f924a07cc2a53df981d956c123ed0fa
Jstarzz <136287579+Jstarzz@users.noreply.github.com>
2026-09-14 10:11:19 -0400

    style(web): load route planning styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './styles.css';
 import './refinements.css';
+import './routing.css';
 import './enrollment.css';
 import App from './App';
 import EnrollmentControl from './EnrollmentControl';
